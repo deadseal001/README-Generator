0adbbfb88236fb23f3e8e3b879e2bde42be56e6d
deadseal001 <liwenbo4936@gmail.com>
2022-10-09 17:42:06 -0500

    add questions

--- Develop/index.js
+++ Develop/index.js
@@ -3,7 +3,90 @@ const inquirer = require('inquirer');
 
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const licenses=require('../assets/license-list.js');
+const licenseName=['none']; 
+licenses.forEach(element =>licenseName.push(element.a));
+licenseName.push('Others');
+const questions = ()=>{
+    return inquirer.prompt([
+        {
+            type:'input',
+            name:'title',
+            message: 'Please Enter the Title of the Project (Required)',
+            validate: titleInput =>{
+                if (titleInput) {
+                    return true;
+                } else {
+                    console.log('Please Enter the Title of the Project!');
+                    return false;
+                }
+            }
+        },
+        {
+            type:'input',
+            name:'description',
+            message:'Please Enter the Description of the Project:',
+            validate: descriptionInput =>{
+                if (descriptionInput) {
+                    return true;
+                } else {
+                    console.log('Please Enter the Description of the Project!');
+                    return false;
+                }
+            }
+        },
+        {
+            type:'input',
+            name:'instinst',
+            message:'Please Enter the Installation Instructions:'
+        },
+        {
+            type: 'input',
+            name: 'usageinfo',
+            message: 'Please Enter the Ussage Informations:'
+        },
+        {
+            type: 'input',
+            name: 'contibution',
+            message: 'Please Enter the Contribution GuideLines:'
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Please Enter the Test Instructions'
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Please Choose the License of the Project:',
+            choices: licenseName
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Please Enter your GitHub Username (Required)',
+            validate: githubInput => {
+                if (githubInput) {
+                return true;
+                } else {
+                console.log('Please enter your GitHub username!');
+                return false;
+                }
+            }
+        },
+        {
+            type:'input',
+            name: 'email',
+            message: 'Please Enter your Email:'
+
+        }
+    ]);
+};
+
+
+//badge info chosenLicense.b
+//let chosenLicense=licenses.find(element=> element.a==='Apache 2.0 License');
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -13,3 +96,5 @@ function init() {}
 
 // Function call to initialize app
 init();
+console.log(licenseName);
+console.log(chosenLicense.b);
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Professional README Generator Starter Code
 
 [How to create a Professional README](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide)
+
+
+
+[![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
